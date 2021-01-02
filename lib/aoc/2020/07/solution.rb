5a32ef251b619c6120adac328aed2460706bf43f
Saverio Trioni <saverio.trioni@gmail.com>
2021-01-02 07:05:47 +0100

    Moved day 7 and 8

---
+++ lib/aoc/2020/07/solution.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'graph'
+
+class Solution
+  def initialize(data)
+    @edge_data = data.lines.flat_map do |line|
+      next [] unless /^(.+) bags contain (\d.+)$/.match(line.strip)
+
+      container, content = Regexp.last_match.captures
+      content.scan(/(\d+) (\w+ \w+) bags?/).map do |weight, color|
+        [container, color, weight.to_i]
+      end
+    end
+  end
+
+  def part_one
+    @edge_data.each_with_object(Graph.new) do |(container, color, _weight), graph|
+      graph.add_edge(color, container)
+    end.node('shiny gold').traverse.to_a.size - 1
+  end
+
+  def part_two
+    @edge_data.each_with_object(Graph.new) do |(container, color, weight), graph|
+      graph.add_edge(container, color, weight)
+    end.node('shiny gold').weight - 1
+  end
+end
---
+++ lib/aoc/2020/08/solution.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require 'set'
+
+class Solution
+  def initialize(data)
+    @instructions = data.each_line.map do
+      _1.split => [k, v]
+      [k.to_sym, v.to_i]
+    end
+  end
+
+  def part_one
+    run[1]
+  end
+
+  def part_two
+    variations.each do |var|
+      run(var) => [status, accumulator]
+      return accumulator if status == :term
+    end
+  end
+
+  private
+
+  State = Struct.new(:pointer, :accumulator)
+
+  def run(overrides = {})
+    visited = Set.new
+
+    state = [0, 0] # pointer, accumulatore
+
+    loop do
+      break [:loop, state[1]] unless visited.add?(state[0])
+      break [:term, state[1]] if state[0] >= @instructions.size
+
+      state = send(*(overrides[state[0]] || @instructions[state[0]]), *state)
+    end
+  end
+
+  def variations
+    @instructions.map.with_index do |instr, idx|
+      case instr
+        in [:nop, d]
+          { idx => [:jmp, d] }
+        in [:jmp, d]
+          { idx => [:nop, d] }
+        else
+        nil
+      end
+    end.compact
+  end
+
+  def nop(_, pointer, accumulator)
+    [pointer + 1, accumulator]
+  end
+
+  def acc(num, pointer, accumulator)
+    [pointer + 1, accumulator + num]
+  end
+
+  def jmp(num, pointer, accumulator)
+    [pointer + num, accumulator]
+  end
+end
